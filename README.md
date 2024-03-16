032c726c0ea4e444ce1d27d424d229ee4b421472
ushka1 <dawid.prajzner01@gmail.com>
2024-03-16 10:45:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Frontend Base
+
+## Description
+
+This is a base project for a frontend application. It includes a basic structure and some useful tools to start a new project.
+
+## Setup
+
+1. Run `npm install` to install the dependencies.
+1. Copy and rename the `.env.template` file to `.env` and set the environment variables.
+1. Run `npm dev` to start the development server.
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    server: {
+      port: 3000,
+      host: '127.0.0.1',
+      hmr: true,
+    },
+  };
+});
