a47ee8902a301647d90e88140c9cda6b9edacaa6
Skotnix <1kamiltomczyk@gmail.com>
2024-03-16 14:04:44 +0100

    added list page with Events (picture, event name, user nick and avatar, date, and num of people) without api ref

--- src/pages/eventList/EventListPage.tsx
+++ src/pages/eventList/EventListPage.tsx
@@ -1,27 +1,79 @@
-import { Box, Typography } from '@mui/material';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 
-export default function EventListPage() {
-  return (
-    <Box>
-      <Typography variant='h4' sx={{ textAlign: 'center', mt: 4 }}>
-        About
-      </Typography>
-      <Typography
-        variant='body1'
-        sx={{ textAlign: 'center', mt: 2, mx: 'auto', maxWidth: 500 }}
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-        possimus accusantium eius magnam, provident blanditiis architecto
-        adipisci laborum ea nobis id commodi eveniet. Dolorem dolore, placeat
-        nostrum, vero doloribus facere repellat eveniet modi consequuntur error
-        sint ex. Rerum a non odit iusto. Deserunt ducimus, officia corporis
-        doloremque temporibus consequatur ea ipsa quis vel facilis nesciunt sunt
-        magnam possimus? Quia magni sapiente minima, nihil fugiat mollitia odio,
-        quae consectetur vero adipisci perferendis veniam laboriosam at quam!
-        Voluptatum facilis deserunt quo quas amet dolorem, eligendi enim
-        nostrum, quisquam, consequuntur repellendus nulla? Modi veniam error
-        quae quia ducimus veritatis, voluptas ex id eveniet.
-      </Typography>
-    </Box>
-  );
-}
+
+export default function BasicList() {
+    return (
+      <Box sx={{ width: '100vw', maxWidth: '100vw', bgcolor: 'background.paper' }}>
+        <nav aria-label="Events">
+          <List>
+          <ListItem disablePadding sx ={{ mb: 1}}>
+                <ListItemButton sx={{ height: '250px', width: '100vw', backgroundImage: `url('/night.jpg')`, position: 'relative', borderRadius: '10px', padding: 0, border: '20px solid white' }}>
+                    <Box sx={{ 
+                    position: 'absolute', 
+                    height: '35%', 
+                    width: '100%', 
+                    bottom: 0, 
+                    backgroundColor: 'white', 
+                    color: 'black', 
+                    display: 'flex', 
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    borderTop: '20px solid white',
+                    paddingLeft: 2,
+                    paddingRight: 2,
+                    }}>
+                    <Box sx={{ alignItems: 'flex-start' }}>
+                        <Typography variant="h5">Event Name</Typography>
+                        <Stack direction="row" spacing={2} alignItems="center">
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 24, height: 24 }} />
+                            <Typography variant="body2">User Name</Typography>
+                        </Stack>
+                    </Box>
+                    <Box sx={{ position: 'absolute', bottom: '5px', right: '2px' }}>
+                         
+                        <Typography variant="body2" align="right">Date: Day/Month/Year</Typography>
+                        <Typography variant="body2" align="right">5 - 10 people</Typography>
+                    </Box>
+                    </Box>
+                    <ListItemText primary/>
+                </ListItemButton>
+                </ListItem>
+                <Divider/> 
+            <ListItem disablePadding sx ={{ mb: 1}}>
+                <ListItemButton sx={{ height: '250px', width: '100vw', backgroundImage: `url('/night.jpg')`, position: 'relative', borderRadius: '10px', padding: 0, border: '20px solid white' }}>
+                    <Box sx={{ 
+                        position: 'absolute', 
+                        height: '25%', 
+                        width: '100%', 
+                        bottom: 0, 
+                        backgroundColor: 'white', 
+                        color: 'black', 
+                        display: 'flex', 
+                        alignItems: 'center', 
+                        justifyContent: 'center',
+                        borderTop: '20px solid white',
+                    }}>
+                    <Typography>Sample Text</Typography>
+                    </Box>
+                <ListItemText primary/>
+                </ListItemButton>
+            </ListItem>
+            <Divider/> 
+          </List>
+        </nav>
+      </Box>
+    );
+  }
